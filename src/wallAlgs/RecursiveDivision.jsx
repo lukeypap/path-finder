5471b9ed454237982c22a97882655a912038ac70
Luke <lukepapaoikonomou@onebright.com>
2022-08-23 14:28:58 +0100

    Started fixing recursive devision alg

--- src/wallAlgs/RecursiveDivision.jsx
+++ src/wallAlgs/RecursiveDivision.jsx
@@ -1,10 +1,9 @@
 export function RecursiveDivision(board, rows, cols) {
   console.log(board); //def working here
   let isHorizontal = true;
-  let currentRows = rows / 2;
-  let currentCols = cols / 2;
+  console.log(rows, cols);
   let wallsInOrder = [];
-  recurseBoard(board, currentRows, currentCols, isHorizontal, wallsInOrder);
+  recurseBoard(board, rows, cols, isHorizontal, wallsInOrder);
   return wallsInOrder;
 }
 
@@ -13,16 +12,32 @@ function recurseBoard(board, rows, cols, isHorizontal, wallsInOrder) {
     return wallsInOrder;
   }
   if (isHorizontal) {
-    for (let i = 0; i < board.length; i++) {
+    for (let i = 0; i < rows - 1; i++) {
       board[i][cols].wall = true;
       wallsInOrder.push(board[i][cols]);
     }
   } else {
-    for (let i = 0; i < board[0].length; i++) {
+    for (let i = 0; i < cols - 1; i++) {
       board[rows][i].wall = true;
       wallsInOrder.push(board[rows][i]);
     }
   }
   console.log(`ROWS: ${rows}, COLS: ${cols}`);
   recurseBoard(board, Math.floor(rows / 2), Math.floor(cols / 2), !isHorizontal, wallsInOrder);
+}
+
+function divide(board, row, col, direction) {
+  if (row < 2 || col < 2) {
+    return;
+  }
+}
+
+function chooseDirection(rows, cols) {
+  if (cols < rows) {
+    return "horizontal";
+  } else if (rows < cols) {
+    return "vertical";
+  } else {
+    return Math.floor(Math.random() * 2) === 0 ? "horizontal" : "vertical";
+  }
 }
